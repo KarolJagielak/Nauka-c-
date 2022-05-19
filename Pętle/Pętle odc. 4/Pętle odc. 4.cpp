678f7d495dce390c15e67e9215c873e05772829b
KarolJagielak <kar.jag44@gmail.com>
2022-05-19 20:01:01 +0200

    komentarz

---
+++ Pętle/Pętle odc. 4/Pętle odc. 4.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <cstdlib>
+
+using namespace std;
+
+string imie; int liczba;
+
+int main()
+{
+	cout << "Podaj imie: ";
+	cin >> imie;
+	cout << "Podaj dodatnia liczbe calkowita: ";
+	cin >> liczba;
+	
+	for (int i = 1; i <= liczba; i++)//petla
+	{
+		cout << i << ". " << imie << endl;
+	}
+
+	
+	
+	
+	
+	return 0;
+}
