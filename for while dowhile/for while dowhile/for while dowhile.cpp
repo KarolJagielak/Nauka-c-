e53d415329b78d9144550c5a1ce44651b3b5debf
KarolJagielak <kar.jag44@gmail.com>
2022-05-21 12:06:10 +0200

    do_while

---
+++ for while dowhile/for while dowhile/for while dowhile.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int populacja=1; int godzin=0;
+
+int main()
+{
+    do 
+    {
+        godzin = godzin++;
+        populacja = populacja * 2;
+
+        cout << "minelo godzin: " << godzin << endl;
+        cout << "liczba bakterii: " << populacja << endl;
+
+
+    } while (populacja <= 1000000000);
+
+    return 0;
+}
